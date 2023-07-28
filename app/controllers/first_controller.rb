294a455d88a797cb3ce708d7b0fe6890563a10ca
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-28 09:51:41 -0400

    update controllers and routes

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,9 +1,13 @@
 class FirstController < ApplicationController
   def welcome
-    render json: {message: "hello there"}
+    render json: { message: "hello there, this is my first message" }
   end
 
-  def yellow
-    render json: {message: "kiwi is a good fruit"}
+  def message2
+    render json: { message: "this is my second messasge test" }
+  end
+
+  def time
+    render json: { message: "it is #{Time.now.strftime("%A at%l:%M %P")}" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/first_route", controller: "first", action: "welcome"
-  get "/kiwi", controller: "first", action: "yellow"
+  get "/second_route", controller: "first", action: "message2"
   get "/current_time", controller: "first", action: "time"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
